54f6efed3fa8dc6cdf4ed90e56845477b6778e6d
Sesslyn J <116268709+Sesslyn041215@users.noreply.github.com>
2023-02-08 15:12:31 +0530

    added js file

---
+++ assets/js/register.js
@@ -0,0 +1,41 @@
+
+  const signUp = e => {
+    e.preventDefault();
+    let name= document.getElementById("name").value
+    let email= document.getElementById("email").value
+    let password=  document.getElementById("password").value
+    let confirm_password=  document.getElementById("confirm_password").value
+
+    let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
+    let exist = user_list.length &&
+        JSON.parse(localStorage.getItem('user_list')).some(data =>data.email.toLowerCase() == email.toLowerCase() );
+    if(!exist){
+        user_list.push({ name:name,  email:email, password:password, confirm_password:confirm_password });
+        localStorage.setItem('user_list', JSON.stringify(user_list));
+        console.log(localStorage.getItem('user_list'));
+        document.querySelector('form').reset();
+        document.getElementById('email').focus();
+        alert('Account created Successfully');
+        location.href = "../../index.html";
+    }
+    else{
+        alert('Sorry the User already Exist!! \n Try with different Email');
+        document.querySelector('form').reset();
+    }
+   
+}
+const signIn = e =>{
+    let password = document.getElementById('password').value;
+    let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
+    let exist = user_list.length &&
+    JSON.parse(localStorage.getItem('user_list')).some(data =>
+        data.password.toLowerCase() == password);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        alert("Login successfully");
+        location.href = "/index.html";
+    }
+    e.preventDefault();
+}
