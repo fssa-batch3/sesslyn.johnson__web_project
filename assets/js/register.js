e93c14bc2b1725c4dd6c9c1afba52b37f43e8ad0
Sesslyn J <116268709+Sesslyn041215@users.noreply.github.com>
2023-02-09 17:53:13 +0530

    Storing Data

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,41 +1,55 @@
-function signUp(e) { 
+
+function signUp(e) {
     e.preventDefault();
-    let name= document.getElementById("name").value
-    let email= document.getElementById("email").value
-    let password=  document.getElementById("password").value
-    let confirm_password=  document.getElementById("confirm_password").value
+    let user_name = document.getElementById("user_name").value,
+        user_email = document.getElementById("user_email").value,
+        user_password = document.getElementById("user_password").value,
+        confirm_password = document.getElementById("confirm_password").value;
 
     let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
-    let exist = user_list.length &&
-        JSON.parse(localStorage.getItem('user_list')).some(data =>data.email.toLowerCase() == email.toLowerCase());
+
+    let exist = user_list.length && 
+        JSON.parse(localStorage.getItem('user_list')).some(data => 
+            data.user_email.toLowerCase() == user_email.toLowerCase()
+        );
+
     if(!exist){
-        user_list.push({name, email, password, confirm_password });
+        user_list.push({ user_name, user_email, 
+                        user_password, confirm_password });
+
         localStorage.setItem('user_list', JSON.stringify(user_list));
+
         console.log(localStorage.getItem('user_list'));
+
         document.querySelector('form').reset();
-        document.getElementById('email').focus();
+        document.getElementById('user_email').focus();
         alert('Account created Successfully');
-        location.href = "../Advertisement/Advertisement.html";
+        location.href = "../../index.html";
+    }
+    
+    else{
+        alert('Sorry the User already Exist!! \n Try with different Phone number or email');
+        document.querySelector('form').reset();
+    }
+    
+}
+
+function signIn(e) {
+    e.preventDefault();
+    let user_password = document.getElementById('user_password').value;
+    
+    let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
+
+    let exist = user_list.length &&
+    JSON.parse(localStorage.getItem('user_list')).some(data => 
+        data.user_password.toLowerCase() == user_password);
+
+    if(!exist){
+        alert("Incorrect login credentials");
+        document.querySelector('form').reset();        
     }
     else{
-        alert('Sorry the User already Exist!! \n Try with different Email');
-        document.querySelector('form').reset();
+        alert("Your login in successful");
+        location.href = "/index.html";  
     }
-  
-}
-function signIn(e){
-    e.preventDefault();
-    let password = document.getElementById('password').value;
-    let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
-    let exist = user_list.length &&
-    JSON.parse(localStorage.getItem('user_list')).some(data =>data.password.toLowerCase() == password);
-        if(!exist){
-            alert("Incorrect login credentials");
-        }
-        else{
-            alert("Your login in successful");
-            location.href = "../Advertisement/Advertisement.html";
-        }
-     
-
 }
