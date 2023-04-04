b6fd511b532654f9ddf99ecbd03b6b10cff0b8f9
Sesslyn J <116268709+Sesslyn041215@users.noreply.github.com>
2023-04-04 10:29:36 +0530

    style : changing the ui of the page

--- assets/comments/seller_header.js
+++ assets/comments/seller_header.js
@@ -6,14 +6,14 @@ console.log(rootPath)
 const beforeLogin = ` 
 <div class="head">
 <div class="main_header">
-    <span class="header" id="header_shop"><a href="${rootPath}/index.html"><img class="main_logo" id="main_logo" src="${rootPath}/assets/images/home_page/Logo.jpg"
-            alt="logo of minimalistic" /></a></span>
+    <span class="header" id="header_shop"><a href="${rootPath}/index.html"><img class="main_logo" id="main_logo"
+                src="${rootPath}/assets/images/home_page/Logo.jpg" alt="logo of minimalistic m" /></a></span>
     <div id="nav_header">
-        <span class="header"><a href="#">
-                <p class="header_para">CHAMBER</p>
+        <span class="header"><a href="${rootPath}/pages/seller/product_list.html">
+                <p class="header_para">PRODUCTS</p>
             </a></span>
-        <span class="header"><a href="${rootPath}/pages/shop.html">
-                <p class="header_para">SHOP</p>
+        <span class="header"><a href="${rootPath}/pages/seller/product_form.html">
+                <p class="header_para">NEW</p>
             </a></span>
         <span class="header"><a href="${rootPath}/pages/about.html">
                 <p class="header_para">ABOUT</p>
@@ -22,26 +22,26 @@ const beforeLogin = `
                 <p class="header_para">ORDERS</p>
             </a></span>
     </div>
- <div class="btn">
-   <a href = "${rootPath}/index.html"><button class="sign_in">Sign In</button></a>
-   <a href = "${rootPath}/index.html"><button id="userLogin" class="log_in">Log In</button> </a>
-</div>
+    <div class="btn">
+        <a href = "${rootPath}/pages/seller/seller_register.html"><button class="sign_in">Sign In</button></a>
+        <a href = "${rootPath}/pages/seller/seller_login.html"><button id="userLogin" class="log_in">Log In</button> </a>
+     </div>
 </div>
 </div>
 `
 
 
-const AfterLogin = ` 
+const AfterLogin = `
 <div class="head">
 <div class="main_header">
     <span class="header" id="header_shop"><a href="${rootPath}/index.html"><img class="main_logo" id="main_logo"
                 src="${rootPath}/assets/images/home_page/Logo.jpg" alt="logo of minimalistic" /></a></span>
     <div class="nav_header">
-        <span class="header"><a href="#">
-                <p class="header_para">CHAMBER</p>
+        <span class="header"><a href="${rootPath}/pages/seller/product_list.html">
+                <p class="header_para">PRODUCTS</p>
             </a></span>
-        <span class="header"><a href="${rootPath}/pages/shop.html">
-                <p class="header_para">SHOP</p>
+        <span class="header"><a href="${rootPath}/pages/seller/product_form.html">
+                <p class="header_para">NEW</p>
             </a></span>
         <span class="header"><a href="${rootPath}/pages/about.html">
                 <p class="header_para">ABOUT</p>
@@ -52,22 +52,23 @@ const AfterLogin = `
     </div>
 
     <button class="btn_delete" id="logOut">Log Out</button>
-    <a href="${rootPath}/pages/profile/profile_new.html">
+    <a href="${rootPath}/pages/profile_new.html">
         <img src="${rootPath}/assets/images/home_page/profile.png" class="profile_img" alt="profile picture" />
     </a>
 </div>
 </div>
+
 `
 
 
-const loginUser = JSON.parse(localStorage.getItem("profile_id"));
+const loginUser = JSON.parse(localStorage.getItem("seller_id"));
 if (loginUser) {
   document.body.insertAdjacentHTML("afterbegin", AfterLogin);
   const userLoginElement = document.getElementById("userLogin");
   userLoginElement?.addEventListener("click", () => document.body.innerHTML = beforeLogin);
   const userLogoutElement = document.getElementById("logOut");
   userLogoutElement?.addEventListener("click", () => {
-    localStorage.removeItem("profile_id");
+    localStorage.removeItem("seller_id");
     document.body.innerHTML = beforeLogin;
   });
 } 
@@ -75,7 +76,7 @@ else {
   document.body.insertAdjacentHTML("afterbegin", beforeLogin);
   const userLoginElement = document.getElementById("logOut");
   userLoginElement?.removeEventListener("click", () => document.body.innerHTML = AfterLogin);
-  localStorage.removeItem("profile_id");
+  localStorage.removeItem("seller_id");
  
 }
 
