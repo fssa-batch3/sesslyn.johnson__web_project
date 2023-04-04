c293563257c7aace8fbcef8816c9b6cae5a28105
Sesslyn J <116268709+Sesslyn041215@users.noreply.github.com>
2023-04-04 09:53:17 +0530

    feat: add a new feautre

--- assets/comments/header.js
+++ assets/comments/header.js
@@ -4,82 +4,78 @@ const rootPath = window.location.origin;
 console.log(rootPath)
 
 const beforeLogin = ` 
-   <header class="header"> 
-        <div>
-            <img class="logo-img" src="${rootPath}/assets/images/Main/casas-logo.png" alt="logo image" />
-        </div>
-        <div class="navbar">
-          <nav>
-            <ul>
-            <li><a href="${rootPath}/pages/chamber.html"><span class="color">C</span>hamber</a></li>
-            <li><a href="${rootPath}/pages/Shop.html"><span class="color">S</span>hop</a></li>
-            <li><a href="${rootPath}/pages/queries.html"><span class="color">A</span>bout</a></li>
-            <li><a href="${rootPath}/pages/liked.html"><span class="color">W</span>ishlist</a></li>
-            <li><a href="${rootPath}/pages/order.html"><span class="color">O</span>rders</a></li> 
-            </ul>
-        </nav>
-       </div> 
-       <div class="inner" id="inner_sign">
-          <button type="submit"><a href="${rootPath}/pages/Seller/Homepage.html">Seller</a></button>
-        </div>
-       <div class="input_div">
-         <div class="inner">
-             <button type="submit"><a href="${rootPath}/pages/Login/Register.html">Sign Up</a></button>
-         </div>
-         <div class="inner">
-           <button type="submit" id="userLogin"><a href="${rootPath}/pages/Login/Login.html">Login</a></button>
-         </div>
-      </div> 
-    </header>
+<div class="head">
+<div class="main_header">
+    <span class="header" id="header_shop"><a href="${rootPath}/index.html"><img class="main_logo" id="main_logo" src="${rootPath}/assets/images/home_page/Logo.jpg"
+            alt="logo of minimalistic" /></a></span>
+    <div id="nav_header">
+        <span class="header"><a href="#">
+                <p class="header_para">CHAMBER</p>
+            </a></span>
+        <span class="header"><a href="${rootPath}/pages/shop.html">
+                <p class="header_para">SHOP</p>
+            </a></span>
+        <span class="header"><a href="${rootPath}/pages/about.html">
+                <p class="header_para">ABOUT</p>
+            </a></span>
+        <span class="header"><a href="${rootPath}/pages/order/order.html">
+                <p class="header_para">ORDERS</p>
+            </a></span>
+    </div>
+ <div class="btn">
+   <a href = "${rootPath}/index.html"><button class="sign_in">Sign In</button></a>
+   <a href = "${rootPath}/index.html"><button id="userLogin" class="log_in">Log In</button> </a>
+</div>
+</div>
+</div>
 `
 
 
 const AfterLogin = ` 
-
-    <header class="header" >
-      <div>
-        <a href="${rootPath}/index.html">
-          <img class="logo-img" src="${rootPath}/assets/images/Main/casas-logo.png" alt="logo image" />
-        </a>
-      </div>
-      <div class="navbar">
-        <nav>
-         <ul>
-           <li><a href="${rootPath}/pages/chamber.html"><span class="color">C</span>hamber</a></li>
-           <li><a href="${rootPath}/pages/Shop.html"><span class="color">S</span>hop</a></li>
-           <li><a href="${rootPath}/pages/queries.html"><span class="color">A</span>bout</a></li>
-           <li><a href="${rootPath}/pages/liked.html"><span class="color">W</span>ishlist</a></li>
-           <li><a href="${rootPath}/pages/order.html"><span class="color">O</span>rders</a></li> 
-         </ul>
-       </nav>
-     </div> 
-     <div class="inner" id="inner_sign">
-      <button type="submit"><a href="${rootPath}/pages/Seller/Homepage.html">Seller</a></button>
-     </div>
-     <div class="inner">
-     <a href="${rootPath}/index.html"><button id="logOut" type="submit">Log Out</button></a>
-      </div>
-    <div class="triple-bar">
-      <a href="${rootPath}/pages/profile.html"><img src="${rootPath}/assets/images/Main/profile (1).png"  alt="profile image"/></a>
+<div class="head">
+<div class="main_header">
+    <span class="header" id="header_shop"><a href="${rootPath}/index.html"><img class="main_logo" id="main_logo"
+                src="${rootPath}/assets/images/home_page/Logo.jpg" alt="logo of minimalistic" /></a></span>
+    <div class="nav_header">
+        <span class="header"><a href="#">
+                <p class="header_para">CHAMBER</p>
+            </a></span>
+        <span class="header"><a href="${rootPath}/pages/shop.html">
+                <p class="header_para">SHOP</p>
+            </a></span>
+        <span class="header"><a href="${rootPath}/pages/about.html">
+                <p class="header_para">ABOUT</p>
+            </a></span>
+        <span class="header"><a href="${rootPath}/pages/order/order.html">
+                <p class="header_para">ORDERS</p>
+            </a></span>
     </div>
-</header>
+
+    <button class="btn_delete" id="logOut">Log Out</button>
+    <a href="${rootPath}/pages/profile/profile_new.html">
+        <img src="${rootPath}/assets/images/home_page/profile.png" class="profile_img" alt="profile picture" />
+    </a>
+</div>
+</div>
 `
 
 
-const loginUser = JSON.parse(localStorage.getItem("user_id"));
+const loginUser = JSON.parse(localStorage.getItem("profile_id"));
 if (loginUser) {
   document.body.insertAdjacentHTML("afterbegin", AfterLogin);
   const userLoginElement = document.getElementById("userLogin");
   userLoginElement?.addEventListener("click", () => document.body.innerHTML = beforeLogin);
   const userLogoutElement = document.getElementById("logOut");
   userLogoutElement?.addEventListener("click", () => {
-    localStorage.removeItem("user_id");
+    localStorage.removeItem("profile_id");
     document.body.innerHTML = beforeLogin;
   });
-} else {
+} 
+else {
   document.body.insertAdjacentHTML("afterbegin", beforeLogin);
   const userLoginElement = document.getElementById("logOut");
   userLoginElement?.removeEventListener("click", () => document.body.innerHTML = AfterLogin);
-  localStorage.removeItem("user_id");
+  localStorage.removeItem("profile_id");
+ 
 }
 
