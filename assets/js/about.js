b770acc2b3b5086adfc0d9c6160f4e4cfec2163b
Sesslyn J <116268709+Sesslyn041215@users.noreply.github.com>
2023-04-04 12:45:20 +0530

    feat: cleared some bugs

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
@@ -17,24 +16,4 @@ function reveal() {
   
   window.addEventListener("scroll", reveal);
 
-=======
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
-  
-  window.addEventListener("scroll", reveal);
-
->>>>>>> bb5721000df383eb6f743760b31676bca2815f2e
   
